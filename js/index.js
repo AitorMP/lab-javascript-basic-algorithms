63b2883ae1c48acb4af3e3432bee5aa120583758
AitorMP <aitor.martinez.pons@gmail.com>
2022-01-15 16:50:31 +0000

    Done iteration 1, 2 & 3.1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 // Iteration 1: Names and Input
 
+let driver = 'Jon';
+  console.log(`The driver\'s name is ${driver}`);
+
+let navigator = 'Nami';
+  console.log (`The navigator\'s name is ${navigator}`);
+
 // Iteration 2: Conditionals
 
+const longerName = 'Nami';
+ console.log(`The navigator has the longest name, it has ${longerName.length} characters`);
 
 // Iteration 3: Loops
+
+console.log(driver.toUpperCase());
